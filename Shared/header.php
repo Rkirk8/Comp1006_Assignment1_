059831dfe197658ab627b74bd3a9c4eb52c8685f
Rielly Kirk <145283535+Rkirk8@users.noreply.github.com>
2024-02-08 21:52:35 -0500

    got the dropdown menu populated by SQL

---
+++ Shared/header.php
@@ -0,0 +1,24 @@
+<!--Shared Header-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo$title; ?></title>
+</head>
+<body>
+    <header>
+        <h1>Barrie RFC Coaches Portal</h1>
+        <!-- Global Nav-->
+        <ol>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="add-player.php">Add New Players</a>
+            </li>
+            <li>
+                <a href="veiw-team.php">View Team</a>
+            </li>
+        </ol>
+    </header>
---
+++ index.php
@@ -0,0 +1,10 @@
+    <!-- shared header -->
+    <?php
+    $title = 'Barrie RFC Coach Portal Home';
+    include('Shared/header.php');
+    ?>
+    <!--Simple Opening message-->
+    <h1>Welcome to the Barrie RFC Coach Portal.</h1>
+    <p>In the Coach Portal you can add players to your team on the add players page, you can also view your team on the veiw team page. </p>
+</body>
+</html>
